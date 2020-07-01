b86df9ce51b6e41bd06d1ef4e381e3920e0f7192
Tim Bird <tbird20d@yahoo.com>
2020-06-30 20:13:30 -0700

    Add final rps.py data for the game
    This was generated by generate-rps-data, and then a few
    tweaks were made by hand.

--- rgdata/rps.py
+++ rgdata/rps.py
@@ -1,17 +1,56 @@
-# trivia data for the red-green game, to be imported by rg.cgi
-# each question has a key that is the question number (as a string)
-# and value that is a list with the question and answer
-#   that is: qdata[question_num][0] = question text
-#            qdata[question_num][1] = answer code
-#            qdata[question_num][2] = answer text
+# rps data for the rock-paper-scissors portion of the red-green game
+# to be imported by rg.cgi
+# this file represents the "host throw" for each numbered round
+
 rps_data = {
+    0: "rock",
     1: "rock",
-    2: "rock",
+    2: "scissors",
     3: "paper",
-    4: "rock",
+    4: "paper",
     5: "scissors",
-    6: "scissors",
-    7: "rock",
+    6: "rock",
+    7: "scissors",
     8: "paper",
-    9: "scissors",
+    9: "paper",
+    10: "scissors",
+    11: "rock",
+    12: "paper",
+    13: "paper",
+    14: "rock",
+    15: "scissors",
+    16: "scissors",
+    17: "scissors",
+    18: "rock",
+    19: "scissors",
+    20: "paper",
+    21: "rock",
+    22: "paper",
+    23: "scissors",
+    24: "rock",
+    25: "scissors",
+    26: "rock",
+    27: "rock",
+    28: "paper",
+    29: "paper",
+    30: "rock",
+    31: "paper",
+    32: "paper",
+    33: "scissors",
+    34: "rock",
+    35: "rock",
+    36: "scissors",
+    37: "rock",
+    38: "rock",
+    39: "paper",
+    40: "rock",
+    41: "rock",
+    42: "paper",
+    43: "paper",
+    44: "paper",
+    45: "paper",
+    46: "scissors",
+    47: "rock",
+    48: "scissors",
+    49: "scissors",
 }
