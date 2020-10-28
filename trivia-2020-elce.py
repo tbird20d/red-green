2dcf72607f3b03d201445e5b10e95667e2d53495
Tim Bird <tbird20d@yahoo.com>
2020-10-28 16:23:33 -0700

    Finalize trivia used for ELCE 2020 closing game
    Signed-off-by: Tim Bird <tbird20d@yahoo.com>

--- trivia-2020-elce.py
+++ trivia-2020-elce.py
@@ -45,8 +45,8 @@ Besides - the current release kernel is 5.9.1, not 5.9.4
 """
 Microsoft has begun shipping the Linux kernel to many of their
 Windows customers.
-          <br>
-          <img src="%(image_url)s/mslogo.jpg" height="100">
+<p>
+<img src="%(image_url)s/mslogo.jpg" height="80">
 """,
 "True",
 "False",
@@ -69,7 +69,7 @@ Source:
 The famous Book of Kells is an "Illuminated Manuscript" created in the
 9th century.  Where can you view it today?
 </td><td>
-<img src="%(image_url)s/book-of-kells.jpg" height="200">
+<img src="%(image_url)s/book-of-kells.jpg" height="150">
 </td></tr></table>
 """,
 
@@ -79,7 +79,7 @@ The famous Book of Kells is an "Illuminated Manuscript" created in the
 "red",
 """
 <table><tr><td>
-<img src="%(image_url)s/trinity-college-library.jpg" height="200">
+<img src="%(image_url)s/trinity-college-library.jpg" height="100">
 </td><td>
 The Book of Kells is housed in the Trinity College Library (but not in the "Long Hall", pictured here).  The book is famous for its elaborate artwork, and is
 considered one of Ireland\'s greatest cultural treasures.
@@ -122,7 +122,7 @@ Sources:
 <table><tr><td>
 What noted science fiction author created the 3 laws of robotics?
 </td><td>
-<img src="%(image_url)s/robot.jfif" height="200">
+<img src="%(image_url)s/robot.jfif" height="150">
 </td></tr></table>
 """,
 "Arthur C. Clark",
@@ -148,7 +148,7 @@ Source:
 
 Isaac Asimov wrote which epic science fiction series of novels?
 <br>
-<img align="center" src="%(image_url)s/asimov.jpeg" height="200">
+<img align="center" src="%(image_url)s/asimov.jpeg" height="100">
 
 """,
 "Dune",
@@ -162,13 +162,15 @@ The Foundation series describes the fall of the galactic empire.
 Frank Herber wrote Dune.  Both series will be available in video form
 in 2021!
 </td><td>
-<img src="%(image_url)s/foundation-book.jpg" height="200">
+<img src="%(image_url)s/foundation-book.jpg" height="150">
 </td></tr></table>
 <p>
 Source:
 <ul>
 <li>https://en.wikipedia.org/wiki/Foundation_(TV_series)
 </ul>
+<p align="center">
+<i>Note: you can <u>read</u> them now!</i>
 
 """,
 ],
@@ -178,7 +180,7 @@ Source:
 The 5.8 kernel consists of 69,325 files and 28442673 lines of code.
 How many files were in the first release of Linux in 1991?
 <p>
-<img src="%(image_url)s/linux-1991.jpg" height="200">
+<img src="%(image_url)s/linux-1991.jpg" height="100">
 """,
 "37 files",
 "88 files",
@@ -203,7 +205,7 @@ Scientists recently demonstrated a device that harvests usable
 electricity from "Brownian current" at room temperature, with no
 moving parts and no heat transfer.
 <p>
-<img src="%(image_url)s/brownian-current.jpeg" height="200">
+<img src="%(image_url)s/brownian-current.jpeg" height="100">
 
 """,
 "True",
@@ -217,14 +219,15 @@ scientists use one-way diodes and the ripple of Brownian current in
 graphene to produce an electrical charge capable of putting a load on
 a resistor.
 <p>
-<img src="%(image_url)s/graphene-electricity-harvester.PNG" height="200">
+<img src="%(image_url)s/graphene-electricity-harvester.PNG" height="150">
 <p>
-
 
 Source:
 <ul>
 <li>https://phys.org/news/2020-10-physicists-circuit-limitless-power-graphene.html
 </ul>
+<p align="center">
+<i>What will those crazy scientists think of next!!</i>
 
 """,
 ],
@@ -233,7 +236,7 @@ Source:
 """
 How many emails were sent to the Linux Kernel Mailing List (LKML) in 2019?
 <p>
-<img src="%(image_url)s/full-mailbox.jpeg" height="200">
+<img src="%(image_url)s/full-mailbox.jpeg" height="100">
 
 """,
 "more than 400,000",
@@ -250,6 +253,8 @@ Source:
 <li>2020 Kernel History Report:
 https://www.linuxfoundation.org/wp-content/uploads/2020/08/2020_kernel_history_report_082720.pdf
 </ul>
+<p align="center">
+<i>It gives new meaning to the phrase "You've got mail!"</i>
 
 """,
 ],
@@ -261,7 +266,7 @@ https://www.linuxfoundation.org/wp-content/uploads/2020/08/2020_kernel_history_r
 What is the official name of the character that "The
 Mandalorian" protects?
 </td><td>
-          <img src="%(image_url)s/baby-yoda.png" height="200">
+          <img src="%(image_url)s/baby-yoda.png" height="150">
 </td></tr></table>
 
 """,
@@ -274,12 +279,15 @@ Mandalorian" protects?
 Although fans named the character "Baby Yoda", the child is never
 referred to by that name in the series.
 <p>
-<img src="%(image_url)s/mandalorian.png" height="200">
+<img src="%(image_url)s/mandalorian.png" height="150">
 <p>
 Source:
 <ul>
 <li>https://www.starwars.com/news/quiz-how-well-do-you-know-the-mandalorian-season-one
 </ul>
+<p align="center">
+<i>Parenting can be difficult - especially when the Empire
+is trying to kill you.</i>
 
 """,
 ],
@@ -305,13 +313,15 @@ A magnon is a quasi-particle, and is a quanta of spin-wave. The waves
 are formed by distortions in the magnetic order of a solid material on
 the quantum level.
 <p>
-<img src="%(image_url)s/magnon-circuit.jpg" height="200">
+<img src="%(image_url)s/magnon-circuit.jpg" height="150">
 <p>
 
 Source:
 <ul>
 <li>https://techxplore.com/news/2020-10-circuit-pure-magnons.html
 </ul>
+<p align="center">
+<i>Admit it. You thought I made those words up.</i>
 """,
 ],
 
@@ -320,7 +330,7 @@ Source:
 <table><tr><td>
 What is the closest that Linux has gotten to the planet Mars?
 </td><td>
-<img src="%(image_url)s/mars.jpg" height="200">
+<img src="%(image_url)s/mars.jpg" height="150">
 </td></tr></table>
 
 """,
@@ -335,11 +345,11 @@ A Tesla car (running Linux?) has an orbit that occasionally gets close
 to Mars.  It recently passed within 8 million kilometers of the
 planet.
 <table><tr><td>
-<img src="%(image_url)s/cubesat-over-mars.jpg" height="150">
+<img src="%(image_url)s/cubesat-over-mars.jpg" height="100">
 </td><td width="20%%">
 &nbsp;
 </td><td>
-<img src="%(image_url)s/Roadster_Earth.png" height="150">
+<img src="%(image_url)s/Roadster_Earth.png" height="100">
 </td></tr></table>
 <p>
 None of the Mars landers have incorporated Linux.
@@ -360,7 +370,7 @@ Sources:
 In 2019, there were over 4,200 contributors to the Linux kernel.  In
 what year did the number of contributors first surpass 1000?
 <p>
-<img src="%(image_url)s/developer-conference.jpg" height="150">
+<img src="%(image_url)s/developer-conference.jpg" height="100">
 
 """,
 "2005",
@@ -388,7 +398,7 @@ https://www.linuxfoundation.org/wp-content/uploads/2020/08/2020_kernel_history_r
 The movie Tenet incorporates elements from which of these ancient
 puzzle devices?
 </td><td>
-<img src="%(image_url)s/tenet_poster_scaled.jpg" height="300">
+<img src="%(image_url)s/tenet_poster_scaled.jpg" height="200">
 </td></tr></table>
 
 """,
@@ -402,8 +412,10 @@ All of the words of the Sator square are used in the movie.  The
 square can be read  forwards and backwards, which ties in with the
 theme of the movie.
 </td><td valign="middle">
-<img src="%(image_url)s/Sator_Square.jpg" height="200">
+<img src="%(image_url)s/Sator_Square.jpg" height="100">
 </td></tr></table>
+<p align="center">
+<i>My understanding is that in Europe you can see movies now.</i>
 
 """,
 ],
@@ -443,6 +455,8 @@ Source
 <li>https://www.theregister.com/2020/06/30/hard_to_find_linux_maintainers_says_torvalds/
 <li>https://lwn.net/Articles/829858/
 </ul>
+<p align="center">
+<i>If you answered "C++", may God have mercy on your soul!</i>
 """,
 ],
 
@@ -452,7 +466,7 @@ Source
 The Civil Infrastructure Platform intends to maintain their Super Long
 Term Support Linux kernel for how many years?
 <p>
-<img src="%(image_url)s/cip-logo.png" height="100">
+<img src="%(image_url)s/cip-logo.png" height="80">
 """,
 "10 years",
 "20 years",
@@ -503,6 +517,8 @@ Source:
 </td><td>
 <img src="%(image_url)s/robocop-boot.png" height="200">
 </td></tr></table>
+<p align="center">
+<i>DOS can run in 640K - Can Linux do that?</i>
 """,
 ],
 
@@ -551,6 +567,9 @@ Source:
 <ul>
 <li>https://arstechnica.com/gadgets/2020/09/nvidia-reportedly-to-acquire-arm-holdings-from-softbank-for-40-billion/
 </ul>
+<p align="center">
+<i>No one has ever offered me $40 billion for anything :-(</i>
+
 """,
 ],
 
@@ -561,7 +580,7 @@ After many years of mostly-successful fundraising, in 2020 Wikipedia
 broke down and accepted government funding, under strict policy that
 it not interfere with editorial decision-making.
 <p>
-<img src="%(image_url)s/wikipedia-logo.jpeg" height="100">
+<img src="%(image_url)s/wikipedia-logo.jpeg" height="80">
 
 """,
 "True",
@@ -580,6 +599,8 @@ Source:
 <ul>
 <li>https://diff.wikimedia.org/2020/09/23/wikipedia-is-getting-a-new-look-for-the-first-time-in-10-years-heres-why/
 </ul>
+<p align="center">
+<i>Keep the information coming!!</i>
 
 """,
 ],
@@ -606,6 +627,9 @@ Sources:
 <li>https://www.theverge.com/2020/9/25/21454772/amazon-ring-car-cam-traffic-stop-police-accountability
 <li>https://techcrunch.com/2020/09/30/a-new-google-assistant-feature-hold-for-me-waits-on-hold-so-you-dont-have-to/
 </ul>
+<p align="center">
+<i>Is it just me, or are people getting lazy?</i>
+
 """,
 ],
 
@@ -668,6 +692,8 @@ Source:
 <ul>
 <li>https://www.computerworld.com/article/3438856/call-me-crazy-but-windows-11-could-run-on-linux.html
 </ul>
+<p align="center">
+<i>Credulity has its limits.</i>
 """,
 ],
 
@@ -696,6 +722,9 @@ Darth Vader breaks his word to Lando Calrisian about letting Princess
 Leia and Wookie stay in cloud city under Lando\'s supervision.
 <p>
 <img src="%(image_url)s/darth-vader-cloud-city.jpg" height="100">
+<p align="center">
+<i>Do NOT invite this guy to dinner.</i>
+
 """,
 ],
 
@@ -723,6 +752,8 @@ Source:
 <li>Tim Bird - I was there
 <li>Also, it was announced in this ELCE\'s keynote address on Monday
 </ul>
+<p align="center">
+<i>Happy Birthday Yocto!!!</i>
 	
 """,
 ],
@@ -752,6 +783,8 @@ Source:
 <ul>
 <li>https://cregit.linuxsources.org/code/5.7/
 </ul>
+<p align="center">
+<i>Linus is such a slacker!</i>
 
 """,
 ],
@@ -775,7 +808,7 @@ ZombieLand is a movie
 </td><td>
 <img src="%(image_url)s/zombieland.jpg" height="100">
 </td><td>
-<font size="+1">&nbsp;&nbsp;is not&nbsp;&nbsp;</font>
+<font size="+1">&nbsp;&nbsp; <b>is not</b> &nbsp;&nbsp;</font>
 </td><td>
 <img src="%(image_url)s/zombieload.jpg" height="100">
 </td></tr></table>
@@ -858,6 +891,8 @@ The kconfig entry for PREEMPT_RT was added in the 5.3 kernel.  See
 kernel/Kconfig.preempt.
 <p>
 <img src="%(image_url)s/shark-with-laser.jpg" height="150">
+<p align="center">
+<i>It's about <u>time</u>!  See what I did there?</i>
 
 """,
 ],
@@ -892,7 +927,7 @@ Source:
 """
 What was Linux first called:
 <p>
-<img src="%(image_url)s/linus.png" height="200">
+<img src="%(image_url)s/linus.png" height="100">
 
 """,
 "Linus\' Unix",
@@ -907,6 +942,7 @@ Source:
 <ul>
 <li>https://en.wikipedia.org/wiki/History_of_Linux
 </ul>
+<p align="center">
 <i>And to think, we could have had the "Embedded Freax Conference"</i>
 
 """,
@@ -937,7 +973,7 @@ signatures at different times.
 The Linux Foundation has become the home to many open source projects.
 How many LF projects are there currently?
 <p>
-<img src="%(image_url)s/Linux-Foundation_logo.png" height="120">
+<img src="%(image_url)s/Linux-Foundation_logo.png" height="80">
 
 """,
 
@@ -961,7 +997,7 @@ I\'m aware of that are not listed.
 On average, how many patches per hour were contributed to the Linux
 5.7 release?
 <p>
-<img src="%(image_url)s/assembly-line.jfif" height="200">
+<img src="%(image_url)s/assembly-line.jfif" height="100">
 
 """,
 "about 9",
@@ -1005,6 +1041,8 @@ Source:
 <ul>
 <li>https://lkml.org/lkml/2020/9/4/1122
 </ul>
+<p align="center">
+<i>That picture was the wrong kind of dispatch!</i>
 """,
 ],
 
@@ -1030,6 +1068,8 @@ Sources:
 <li>https://www.sciencealert.com/california-has-just-reported-a-case-of-human-plague
 <li>https://www.bbc.com/future/article/20200806-the-biblical-east-african-locust-plagues-of-2020
 </ul>
+<p align="center">
+<i>We'd like a break now.</i>
 """,
 ]
 }
